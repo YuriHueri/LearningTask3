1d8651480c6af358859abcd8dd67e11dae395b07
YuriHueri <113897111+YuriHueri@users.noreply.github.com>
2022-10-15 21:33:20 +0800

    Add files via upload

---
+++ College.java
@@ -0,0 +1,18 @@
+public class College
+
+    { String College;
+        public College()
+    {
+
+    }
+
+            public String getCollege()
+    {
+        return College;
+    }
+
+    public void setCollege(String College)
+    {
+        this.College = College;
+    }
+}
---
+++ Subject.java
@@ -0,0 +1,16 @@
+public class Subject {
+    String subject;
+
+    public Subject() {
+
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
+
